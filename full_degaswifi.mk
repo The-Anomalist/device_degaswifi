2d2f16653d6462766bb9af75a013897873b1b117
sub77 <sub77@ymail.com>
2015-09-02 17:04:40 +0200

    full base
    Change-Id: I4b0bfd9724cd5bb8f4381956c4ae1b8434ed0557

--- full_degaswifi.mk
+++ full_degaswifi.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_degaswifi
